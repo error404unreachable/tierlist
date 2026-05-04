8e53581874a8c9ddee166473967db55a8affeaef
error404unreachable <126827750+error404unreachable@users.noreply.github.com>
2026-05-04 15:54:28 +0000

    added container divs and stuff

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 //im ht1 in dropping projects halfway through lol
 
+console.log(window.innerWidth);
+
 document.addEventListener(onmouseover, document.getElementsByClassName("links").style.background = "#333333");
